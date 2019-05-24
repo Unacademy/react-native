e66702ae827b297872e36fb6c4783fa9a5da8d44
Sai Krishna <m.s.krishna310@gmail.com>
2019-05-24 17:07:27 +0530

    reverted c++ changes

--- ReactAndroid/src/main/jni/Application.mk
+++ ReactAndroid/src/main/jni/Application.mk
@@ -1,4 +1,3 @@
-APP_DEPRECATED_HEADERS := true
 APP_BUILD_SCRIPT := Android.mk
 
 APP_ABI := armeabi-v7a x86
@@ -8,7 +7,7 @@ APP_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 NDK_MODULE_PATH := $(APP_MK_DIR)$(HOST_DIRSEP)$(THIRD_PARTY_NDK_DIR)$(HOST_DIRSEP)$(REACT_COMMON_DIR)$(HOST_DIRSEP)$(APP_MK_DIR)first-party
 
-APP_STL := c++_static
+APP_STL := gnustl_shared
 
 # Make sure every shared lib includes a .note.gnu.build-id header
 APP_LDFLAGS := -Wl,--build-id
